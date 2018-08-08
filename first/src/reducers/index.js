b23c4f7e285f65482d1fd63bbbf39f4e4948a594
sepuckett86 <sepuckett86@gmail.com>
2018-08-08 12:06:46 +0200

    split reducers

--- first/src/reducers/index.js
+++ first/src/reducers/index.js
@@ -1,34 +1,101 @@
 // Import variables used by actions from actions file
-import { LOGIN, LOGOUT, ADDTEXT } from '../actions/index';
+import { LOGIN, LOGOUT, ADDTEXT, AUTH,
+  ADDGMINDER, TOGGLEGMINDER } from '../actions/index';
 
 
 // Declare initial state here
 const initialState = {
   authenticated: false,
-  text: ''
+  text: '',
+  gminders: [
+    {
+      id: 1,
+      mainResponse: 'Hello There!',
+      selected: false
+    },
+    {
+      id: 2,
+      mainResponse: 'I ate a burrito today.',
+      selected: false
+    },
+  ]
 }
 
-function myApp(state = initialState, action) {
-  switch (action.type) {
+// Have individual functions for each part of state
+function authenticated(state = false, action) {
+  switch(action.type) {
     case LOGIN:
       console.log(action.text)
-      return Object.assign({}, state, {
-        authenticated: true
-      })
+      return true
     case LOGOUT:
+    console.log(action.text)
+      return false
+    case AUTH:
       console.log(action.text)
-      return Object.assign({}, state, {
-        authenticated: false
-      })
+      return !state
+    default:
+      return state
+  }
+}
+
+function text(state = '', action) {
+  switch(action.type) {
     case ADDTEXT:
-      const text = action.text
-      return Object.assign({}, state, {
-        text: text
+      console.log(action.text)
+      return action.text
+    default:
+      return state
+  }
+}
+
+function gminders(state = [], action) {
+  switch (action.type) {
+    case ADDGMINDER:
+      const id = state.length + 1;
+      return [
+        ...state,
+        {
+          id: id,
+          mainResponse: action.mainResponse,
+          selected: false
+        }
+      ]
+    case TOGGLEGMINDER:
+      return state.map((gminder, index) => {
+        if (index === action.index) {
+          return Object.assign({}, gminder, {
+            selected: !gminder.selected
+          })
+        }
+        return gminder
       })
     default:
       return state
   }
 }
 
+// Have function tying together all functions pertaining
+// to parts of state
+function myApp(state = initialState, action) {
+  return {
+    authenticated: authenticated(state.authenticated, action),
+    text: text(state.text, action),
+    gminders: gminders(state.gminders, action)
+  }
+}
 
+// This is equivalent to the above function:
+/*
+import { combineReducers } from 'redux'
+​
+const myApp = combineReducers({
+  authenticated,
+  text,
+  gminders
+})
+​
+export default myApp
+*/
+
+// Export this tying-together function
 export default myApp
