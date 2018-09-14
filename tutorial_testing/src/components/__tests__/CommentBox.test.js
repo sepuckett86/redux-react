d1bc2bcf29382d9b5f4dcca394f655f984cc4516
sepuckett86 <sepuckett86@gmail.com>
2018-09-14 11:24:47 +0200

    update CommentBox tests

--- tutorial_testing/src/components/__tests__/CommentBox.test.js
+++ tutorial_testing/src/components/__tests__/CommentBox.test.js
@@ -16,7 +16,26 @@ afterEach(() => {
   wrapped.unmount();
 })
 
+// toEqual is matcher statement
 it('has a text area and a button', () => {
   expect(wrapped.find('textarea').length).toEqual(1);
   expect(wrapped.find('button').length).toEqual(1);
 });
+
+it('has a text area that users can type in', () => {
+  wrapped.find('textarea').simulate('change', {
+    target: { value: 'new comment'}
+  });
+  // Because setState is asynchronous,
+  // need to force a rerender before
+  // we test for change
+  wrapped.update();
+  // Check that value property is updated correctly on change
+  expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+});
+
+it('empties text area upon form submission', () => {
+  wrapped.find('form').simulate('submit');
+  wrapped.update();
+  expect(wrapped.find('textarea').prop('value')).toEqual('');
+})
