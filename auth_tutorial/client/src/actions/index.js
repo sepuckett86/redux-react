f76d8b86677cf21baa296015664f6561a40136ec
sepuckett86 <sepuckett86@gmail.com>
2018-10-24 16:29:45 +0200

    add signin and style header

--- auth_tutorial/client/src/actions/index.js
+++ auth_tutorial/client/src/actions/index.js
@@ -10,8 +10,31 @@ export const signup = (formProps, callback) => async dispatch => {
     const response = await axios.post('http://localhost:3090/signup', formProps);
 
     dispatch({ type: AUTH_USER, payload: response.data.token });
+    localStorage.setItem('token', response.data.token);
     callback();
   } catch (e) {
     dispatch({ type: AUTH_ERROR, payload: 'Email in use' });
+  }
+};
+
+// Normal, synchronous action creator
+export const signout = () => {
+  localStorage.removeItem('token');
+
+  return {
+    type: AUTH_USER,
+    payload: ''
+  }
+}
+
+export const signin = (formProps, callback) => async dispatch => {
+  try {
+    const response = await axios.post('http://localhost:3090/signin', formProps);
+
+    dispatch({ type: AUTH_USER, payload: response.data.token });
+    localStorage.setItem('token', response.data.token);
+    callback();
+  } catch (e) {
+    dispatch({ type: AUTH_ERROR, payload: 'Invalid login credentials' });
   }
 };
---
+++ auth_tutorial/client/src/components/auth/Signout.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
+
+class Signout extends Component {
+  componentDidMount() {
+    this.props.signout();
+  }
+  render() {
+    return <div>Bye Bye, you are signed out</div>
+  }
+}
+
+export default connect(null, actions)(Signout);
