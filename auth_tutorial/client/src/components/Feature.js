4c3d37b6bf261d04ee4b98042003328bfb039d22
sepuckett86 <sepuckett86@gmail.com>
2018-10-24 15:30:20 +0200

    add client ad signup functionality and redux-thunk

---
+++ auth_tutorial/client/src/components/Feature.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Feature extends Component {
+  render() {
+    return <div>Secret Stuff</div>
+  }
+}
+
+export default Feature;
--- auth_tutorial/server/index.js
+++ auth_tutorial/server/index.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const router = require('./router');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 
@@ -15,6 +16,8 @@ mongoose.connect('mongodb://localhost:auth/auth', { useNewUrlParser: true });
 // App setup
 // Middleware
 app.use(morgan('combined'));
+// allows requests from anywhere with cors -- security risk
+app.use(cors());
 app.use(bodyParser.json({ type: '*/*'}));
 // Call our router
 router(app);
